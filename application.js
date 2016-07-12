79d11633526efcdf000ad73b8a3b7f32957be45d
rnbronstein <rnbronstein@gmail.com>
2016-07-12 03:01:04 -0400

    added soundcloud api integration

--- application.js
+++ application.js
@@ -15,10 +15,14 @@ function onClick(){
     link.addEventListener("click", function(){
       var url = this.id + '.html';
       toggleSelected(this);
+      var _this = this;
       getPage(url).then(function(response){
         fadeOut('content', 9);
         appendResponse(response);
         fadeIn('content', 0);
+        if (_this.id === 'media') {
+          loadMedia();
+        }
       });
     });
   });
@@ -74,3 +78,54 @@ function fadeIn(id, value){
     return;
   }
 }
+
+//soundcloud
+function loadMedia(){
+  var url = 'https://api.soundcloud.com/users/47262707/tracks?client_id=9e76b1f059ce51b6a6c29c18f5b2858e'; //all tracks by composer
+  getSoundCloud(url);
+}
+
+
+function getSoundCloud(url){
+  getPage(url).then(function(response){
+    var allTracks = JSON.parse(response);
+    allTracks.forEach(function(track){
+      handleTrack(track);
+    })
+  })
+}
+
+function handleTrack(track){
+  var title = track.title;
+  var description = track.description;
+  if (track.streamable) {
+    var client_id = '9e76b1f059ce51b6a6c29c18f5b2858e';
+    var stream = track.stream_url + `?client_id=${client_id}`;
+  }
+  if (track.artwork_url) {
+    var image = track.artwork_url;
+  } else {
+    var image = 'https://radd.it/img/music.jpg';
+  }
+  appendTrack(title, description, stream, image);
+}
+
+function appendTrack(title, description, stream, image){
+  var trackList = document.querySelector('#audio');
+  var trackItem = document.createElement('li');
+  trackItem.classList.add('track-item')
+  var track = document.createElement('li');
+  track.classList.add('track');
+  var trackImage = document.createElement('li');
+  trackImage.innerHTML = `<img class="track-image" src="${image}">`;
+  track.innerHTML = `<h4 class="track-title">${title}</h4><p class="track-description">${description}</p><audio controls><source src="${stream}" type="audio/mpeg"></audio>`;
+  trackItem.appendChild(trackImage);
+  trackItem.appendChild(track);
+  trackList.appendChild(trackItem);
+}
+
+// function appendImage(track, image){
+//   var imageItem = document.createElement('li');
+//
+//   track.appendChild(imageItem);
+// }
